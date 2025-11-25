718f4e316c099f2f874903134625ac79c1f29afb
SP2125 <pessoasarah253@gmail.com>
2025-11-25 20:26:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Rolagem suave ao clicar no menu
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute("href"));
-        target.scrollIntoView({ behavior: "smooth" });
-    });
-});
